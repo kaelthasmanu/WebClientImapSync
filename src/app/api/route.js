f4e6517e12e2e97c55af820a352b8a915190dd00
Manuel <manuelalberto.gorrin@gmail.com>
2023-08-24 20:25:18 -0400

    fix error

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,32 +3,32 @@ import { spawn } from 'node:child_process'
 
 export async function GET(request) {
     const ls = spawn("ls", ["-la"]);
-
+    let value = 0
+    
     ls.stdout.on("data", data => {
         console.log(`stdout: aaaaa${data}`);
-        console.log('Request object:', request); // Check the structure of the Request object
-        console.log('Request headers:', request.headers); // Check if headers property is defined
-        return NextResponse.json({message: "asd"})
+        value = 1
     });
 
     ls.stderr.on("data", data => {
         console.log(`stderr: ${data}`);
-        console.log('Request object:', request); // Check the structure of the Request object
-        console.log('Request headers:', request.headers); // Check if headers property is defined
-        return NextResponse.json({message: data})
+        value = 0
     });
 
     ls.on('error', (error) => {
         console.log(`error: ${error.message}`);
-        console.log('Request object:', request); // Check the structure of the Request object
-        console.log('Request headers:', request.headers); // Check if headers property is defined
-        return NextResponse.json({message: error.message})
+        value = 0
     });
 
     ls.on("close", code => {
         console.log(`child process exited with code ${code}`);
-        console.log('Request object:', request); // Check the structure of the Request object
-        console.log('Request headers:', request.headers); // Check if headers property is defined
-        return NextResponse.json({message: code})
+        value = 0
     });
+    if(value === 0){
+        return NextResponse.json({message: "Success"})
+    }
+    else{
+        return NextResponse.json({message: "Error"})
+    }
+    
 }
