b0eefd43480a30b4537d1ebe073d46fc0a40e80a
Manuel <manuelalberto.gorrin@gmail.com>
2023-08-25 01:05:39 -0400

    new update :)

--- src/app/components/InputEmailComponent.jsx
+++ src/app/components/InputEmailComponent.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {Input} from "@nextui-org/react";
-import {EyeFilledIcon} from "./EyeFilledIcon";
-import {EyeSlashFilledIcon} from "./EyeSlashFilledIcon";
 
 export default function InputEmailComponent() {
     const [value, setValue] = React.useState("manuel.gorrin@umcc.cu");
